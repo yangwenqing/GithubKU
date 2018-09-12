b71d50669a5f9be1cf54eba15506239f5583ff50
YANG <YANG@LAPTOP-EU5UO34B>
2018-09-12 17:07:36 +0800

    第一次提交

---
+++ Homework1/program1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            Console.Write("Please input numberone:");
+            s = Console.ReadLine();
+            int a = Int32.Parse(s);
+            Console.Write("Please input numbertwo:");
+            s = Console.ReadLine();
+            int b = Int32.Parse(s);
+            Console.Write("所求的积为:{0}", a * b);
+        }
+    }
+}
---
+++ Homework1/program2/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = "";
+            s = textBox1.Text;
+            int a = Int32.Parse(s);
+            s = textBox2.Text;
+            int b = Int32.Parse(s);
+            int Answer = a * b;
+            label1.Text = "所求两数之积为：" + Answer;
+        }
+    }
+}
