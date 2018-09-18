da1451d6515ccc775d7b1f73bb3461b8f194b5e7
YANG <YANG@LAPTOP-EU5UO34B>
2018-09-19 00:18:32 +0800

    修改

--- Homewoek2/Question6/Program.cs
+++ Homewoek2/Question6/Program.cs
@@ -8,47 +8,28 @@ namespace Question6
 {
     class Program
     {
-      static Boolean Sushu(int n)//判断素数
-        {
-            for(int i =2;i<n;i++)
-            {
-                if (n % i == 0)
-                    goto Outer;
-            }
-            return true;
-            Outer: return false;
-        }
         static void Main(string[] args)
         {
             string s = "";
             Console.Write("Please input a number:");
             s = Console.ReadLine();
             int Mynumber = Int32.Parse(s);
-
-            if (Sushu(Mynumber) == true)
+            int a = Mynumber;
+            for (int i = 2; i <a; i++)
             {
-                Console.WriteLine(Mynumber);
-            }
-            for (int i = 2; i < Mynumber; i++)
-            {
-                if (Mynumber % i == 0)
+              while(Mynumber % i == 0 && i != Mynumber)
                 {
-                    if (i == 2)
-                    {
-                        Console.Write(i + " ");
-                    }
-                    else
-                    {
-                        for (int j = 2; j < i; j++)
-                        {
-                            if (i % j == 0)
-                                goto Out;
-                        }
-                        Console.Write(i + " ");
-                        Out:;
-                    }
+                    Console.Write(i + " ");
+                    Mynumber = Mynumber / i;
+                }
+              while (Mynumber % i == 0 && i == Mynumber)
+                { 
+                    Console.Write(i + " ");
+                    Mynumber = Mynumber / i;
                 }
             }
+       
+
         }
     }
 }
