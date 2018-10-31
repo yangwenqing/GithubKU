ba3223ba1439fc3a9f5e37f13ba83141d853bd91
YANG <YANG@LAPTOP-EU5UO34B>
2018-11-01 00:54:24 +0800

    第七次提交

---
+++ Homework7/MyForm/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace MyForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.queryInput = new System.Windows.Forms.TextBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.customerDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.amountDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.customerDataGridViewTextBoxColumn,
+            this.amountDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.bindingSource1;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(716, 450);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.PanSouth;
+            this.button1.Location = new System.Drawing.Point(3, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 215);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "添加订单";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, -7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 236);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "删除订单";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(639, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(77, 450);
+            this.panel2.TabIndex = 4;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(77, 218);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 218);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(77, 232);
+            this.panel3.TabIndex = 4;
+            // 
+            // queryInput
+            // 
+            this.queryInput.Dock = System.Windows.Forms.DockStyle.Top;
+            this.queryInput.Location = new System.Drawing.Point(0, 0);
+            this.queryInput.Name = "queryInput";
+            this.queryInput.Size = new System.Drawing.Size(639, 21);
+            this.queryInput.TabIndex = 5;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.panel5);
+            this.panel4.Controls.Add(this.queryInput);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(0, 338);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(639, 112);
+            this.panel4.TabIndex = 6;
+            // 
+            // button
+            // 
+            this.button.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button.Location = new System.Drawing.Point(0, 0);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(639, 23);
+            this.button.TabIndex = 6;
+            this.button.Text = "根据客户名查询";
+            this.button.UseVisualStyleBackColor = false;
+            this.button.Click += new System.EventHandler(this.queryInput_Click);
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.button4);
+            this.panel5.Controls.Add(this.button3);
+            this.panel5.Controls.Add(this.button);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(0, 21);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(639, 91);
+            this.panel5.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button3.Location = new System.Drawing.Point(0, 48);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(639, 43);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "修改订单";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // customerDataGridViewTextBoxColumn
+            // 
+            this.customerDataGridViewTextBoxColumn.DataPropertyName = "Customer";
+            this.customerDataGridViewTextBoxColumn.HeaderText = "Customer";
+            this.customerDataGridViewTextBoxColumn.Name = "customerDataGridViewTextBoxColumn";
+            this.customerDataGridViewTextBoxColumn.Width = 400;
+            // 
+            // amountDataGridViewTextBoxColumn
+            // 
+            this.amountDataGridViewTextBoxColumn.DataPropertyName = "Amount";
+            this.amountDataGridViewTextBoxColumn.HeaderText = "Amount";
+            this.amountDataGridViewTextBoxColumn.Name = "amountDataGridViewTextBoxColumn";
+            this.amountDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // bindingSource1
+            // 
+            this.bindingSource1.DataSource = typeof(ordertest.Order);
+            this.bindingSource1.CurrentChanged += new System.EventHandler(this.bindingSource1_CurrentChanged);
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button4.Location = new System.Drawing.Point(0, 23);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(639, 25);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "返回第一页";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 450);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn customerDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn amountDataGridViewTextBoxColumn;
+        private System.Windows.Forms.TextBox queryInput;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Homework7/MyForm/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ordertest;
+
+namespace MyForm
+{
+    public partial class Form1 : Form
+    {
+        public List<Order> orders = new List<Order>();
+        public string KeyWord { set; get; }
+        public Form1()
+        {
+            InitializeComponent();
+            bindingSource1.DataSource = orders;
+            queryInput.DataBindings.Add("Text", this, "KeyWord");
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form form1 = new Form();
+          
+            Button Mybutton1 = new Button();
+            TextBox t1 = new TextBox();
+            TextBox t2 = new TextBox();
+            TextBox t3 = new TextBox();
+            TextBox t4 = new TextBox();
+            TextBox t5 = new TextBox();
+            TextBox t6 = new TextBox();
+            TextBox t7 = new TextBox();
+            Label l1 = new Label();
+            Label l2 = new Label();
+            Label l3 = new Label();
+            Label l4 = new Label();
+            Label l5 = new Label();
+            Label l6 = new Label();
+            Label l7 = new Label();
+            Mybutton1.Text = "确认添加";
+            l1.Text = "请输入订单号";
+            l1.Location = new Point(10, 10);
+            t1.Location = new Point(l1.Left, l1.Height + l1.Top + 10);
+            l2.Text = "请输入顾客名称";
+            l2.Location = new Point(l1.Left, t1.Height + t1.Top + 10);
+            t2.Location = new Point(l1.Left, l2.Height + l2.Top + 10);
+            l3.Text = "请输入商品数量";
+            l3.Location = new Point(l1.Left, t2.Height + t2.Top + 10);
+            t3.Location = new Point(l1.Left, l3.Height + l3.Top + 10);
+            l4.Text = "请输入商品名称";
+            l4.Location = new Point(l1.Left, t3.Height + t3.Top + 10);
+            t4.Location = new Point(l1.Left, l4.Height + l4.Top + 10);
+            l5.Text = "请输入商品单价";
+            l5.Location = new Point(l1.Left, t4.Height + t4.Top + 10);
+            t5.Location = new Point(l1.Left, l5.Height + l5.Top + 10);
+            l6.Text = "请输入商品序号";
+            l6.Location = new Point(l1.Left, t5.Height + t5.Top + 10);
+            t6.Location =new Point(l1.Left, l6.Height + l6.Top + 10);
+            l7.Text = "请输入顾客序号";
+            l7.Location = new Point(l1.Left, t6.Height + t6.Top + 10);
+            t7.Location = new Point(l1.Left, l7.Height + l7.Top + 10);
+            Mybutton1.Location = new Point(l1.Left, t7.Height + t7.Top + 10);
+            form1.Text = "订单添加";
+
+            form1.FormBorderStyle = FormBorderStyle.FixedDialog;
+            form1.Size = new Size(500, 600);
+            form1.StartPosition = FormStartPosition.CenterScreen;
+            form1.Controls.Add(l1);
+            form1.Controls.Add(l2);
+            form1.Controls.Add(l3);
+            form1.Controls.Add(l4);
+            form1.Controls.Add(l5);
+            form1.Controls.Add(t1);
+            form1.Controls.Add(t2);
+            form1.Controls.Add(t3);
+            form1.Controls.Add(t4);
+            form1.Controls.Add(t5);
+            form1.Controls.Add(t6);
+            form1.Controls.Add(t7);
+            form1.Controls.Add(l6);
+            form1.Controls.Add(l7);
+            form1.Controls.Add(Mybutton1);
+            Mybutton1.DialogResult = DialogResult.OK;
+            form1.AcceptButton = Mybutton1;
+            form1.ShowDialog();
+
+
+            string s1 = t1.Text;
+            string s2 = t2.Text;
+            string s3 = t3.Text;
+            string s4 = t4.Text;
+            string s5 = t5.Text;
+            string s6 = t6.Text;
+            string s7 = t7.Text;
+            uint num1 = 0;
+            double num2 = 0;
+
+            num1 = UInt32.Parse(s6);
+            num2 = Double.Parse(s5);
+            ordertest.Goods good = new ordertest.Goods(num1, s4, num2);
+            num1 = UInt32.Parse(s7);
+            ordertest.Customer cu = new ordertest.Customer(num1, s2);
+            num1 = UInt32.Parse(s3);
+            ordertest.OrderDetail od = new ordertest.OrderDetail(1, good, num1);
+            num1 = UInt32.Parse(s1);
+            ordertest.Order or = new ordertest.Order(num1, cu);
+            or.AddDetails(od);
+
+            if (form1.DialogResult==DialogResult.OK)
+            {
+                orders.Add(or);
+                form1.Dispose();
+                bindingSource1.DataSource = null;
+                bindingSource1.DataSource = orders;
+            }
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form form2 = new Form();
+            TextBox t1 = new TextBox();
+            Button b1 = new Button();
+            Label l1 = new Label();
+            l1.Text = "请输入订单号";
+            l1.Location = new Point(30, 40);
+            t1.Location =new Point (l1.Left, l1.Height + l1.Top + 40);
+            b1.Text = "确认删除";
+            b1.Location = new Point(l1.Left, t1.Height + t1.Top + 40);
+            form2.Controls.Add(l1);
+            form2.Controls.Add(t1);
+            form2.Controls.Add(b1);
+            b1.DialogResult = DialogResult.OK;
+            form2.AcceptButton = b1;
+            form2.ShowDialog();
+            uint num = 0;
+            string s = t1.Text;
+            num = UInt32.Parse(s);
+            if(form2.DialogResult == DialogResult.OK)
+            {
+                int m = -1;
+                for (int n = 0; n < orders.Count(); n++)
+                {
+                    if (orders[n].Id==num)
+                    {
+                        m = n;
+                    }
+                }
+                if(m>=0)
+                {
+                    orders.Remove(orders[m]);
+                }
+                form2.Dispose();
+                    bindingSource1.DataSource = null;
+                    bindingSource1.DataSource = orders;
+               
+            }
+        }
+
+        private void queryInput_Click(object sender, EventArgs e)
+        {
+            bindingSource1.DataSource = null;
+            bindingSource1.DataSource =
+              orders.Where(s => s.Customer.Name==KeyWord);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form form3 = new Form();
+            TextBox t1 = new TextBox();
+            TextBox t2 = new TextBox();
+            Button b1 = new Button();
+            Label l1 = new Label();
+            Label l2 = new Label();
+           
+            l1.Text = "请输入订单号";
+            l1.Location = new Point(30, 20);
+            t1.Location = new Point(l1.Left, l1.Height + l1.Top + 20);
+            l2.Text = "修改后的姓名";
+            l2.Location = new Point(l1.Left, t1.Height + t1.Top + 20);
+            t2.Location = new Point(l1.Left, l2.Height + l2.Top + 20);
+            b1.Text = "确认修改";
+            b1.Location = new Point(l1.Left, t2.Height + t2.Top + 20);
+            form3.Controls.Add(l1);
+            form3.Controls.Add(t1);
+            form3.Controls.Add(l2);
+            form3.Controls.Add(t2);
+            form3.Controls.Add(b1);
+            b1.DialogResult = DialogResult.OK;
+            form3.AcceptButton = b1;
+            form3.ShowDialog();
+            uint num = 0;
+            string s = t1.Text;
+            num = UInt32.Parse(s);
+            int m = -1;
+            for(int n =0;n<orders.Count();n++)
+            {
+                if (orders[n].Id==num)
+                {
+                    m = n;
+                }
+            }
+            s = t2.Text;
+            if (form3.DialogResult == DialogResult.OK)
+            {
+               if(m>=0)
+                {
+                    orders[m].Customer.Name = s;
+                }
+                form3.Dispose();
+                bindingSource1.DataSource = null;
+                bindingSource1.DataSource = orders;
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            bindingSource1.DataSource = null;
+            bindingSource1.DataSource = orders;
+        }
+    }
+}
